4532623aa0f096052ad37cb92b5e9b315758b5b0
JoshuaLamusga <joshlamusga@gmail.com>
2020-11-19 03:35:18 +0000

    Deploying to gh-pages from  @ a28a552795aa6dc44401b82b8486a37e70a65d9e 🚀

--- src/common/redux/reduxTools.ts
+++
@@ -1,9 +0,0 @@
-let uniqueActionType = Number.MIN_SAFE_INTEGER;
-
-/**
- * Action types must be unique across all reducers.
- * This assigns a globally unique id every time it's called.
- */
-export const getActionGuid = () => {
-  return (++uniqueActionType).toString();
-};
